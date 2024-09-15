2498b35502d2094329a46bedd5825886af21772a
Prabesh Shrestha <shresthaprabesh155@gmail.com>
2024-09-15 13:27:42 +0545

    Two Approaches of Multithreading

---
+++ src/Lab1/TwoApproachOfMultithreading.java
@@ -0,0 +1,28 @@
+package Lab1;
+
+public class TwoApproachOfMultithreading {
+public static void main(String[] args) {
+	ApproachOne a1 = new ApproachOne();
+	a1.start();
+	
+	ApproachTwo a2 = new ApproachTwo();
+	Thread t1 = new Thread(a2);
+	t1.start();
+}
+}
+
+class ApproachOne extends Thread{
+	public void run() {
+		for (int i = 1; i <= 2; i++) {
+            System.out.println("Thread from ApproachOne class: " + i);
+        }
+	}
+}
+
+class ApproachTwo implements Runnable{
+	public void run() {
+		for (int i = 1; i <= 2; i++) {
+            System.out.println("Thread from ApproachTwo class: " + i);
+        }
+	}
+}
